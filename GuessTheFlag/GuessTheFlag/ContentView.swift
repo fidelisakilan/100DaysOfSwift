1e1c828a2168156081e38cfaa39db47fa1f097b2
Fidelis Akilan <fidelisakilan@proton.me>
2025-05-23 18:51:21 -0500

    day 22: updated design for guess the flag

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -12,45 +12,78 @@ struct ContentView:View {
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var score = 0
+    @State private var tappedNumber = 0
     
     var body: some View {
         ZStack {
-            LinearGradient(colors: [.blue,.black], startPoint: .top, endPoint: .bottom)
-                .ignoresSafeArea()
-            VStack(spacing: 30) {
-                VStack {
-                    Text("Tap the flag of")
-                        .foregroundStyle(.white)
-                        .font(.subheadline.weight(.heavy))
-                    Text(countries[correctAnswer])
-                        .foregroundStyle(.white)
-                        .font(.largeTitle.weight(.semibold))
-                }
-                ForEach(0..<3) { number in
-                    Button {
-                        flagTapped(number)
-                    } label: {
-                        Image(countries[number])
-                            .clipShape(.capsule)
-                            .shadow(radius: 5)
+            RadialGradient(stops: [
+                .init(color: Color(red:0.1, green:0.2, blue: 0.65), location: 0.3),
+                .init(color: Color(red:0.76, green:0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.white)
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .foregroundStyle(.white)
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 5)
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.ultraThinMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                Spacer()
+                Spacer()
+                Text("Score: \(score)")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                Spacer()
             }
+            .padding()
         }
         .alert(scoreTitle, isPresented: $showingScore) {
-            Button("Continue",action: askQuestion)
+            Button("Continue",role:.cancel, action: askQuestion)
+            Button("Reset",role: .destructive,action: reset)
         } message: {
-            Text("Your score is ???")
+            Text("""
+"Wrong! That's flag of \(countries[tappedNumber])"
+Your score is \(score)
+""")
         }
     }
     
     func flagTapped(_ number: Int) {
+        tappedNumber = number
         if number == correctAnswer {
             scoreTitle = "Correct"
+            score += 50
+            askQuestion()
         } else {
             scoreTitle = "Wrong"
+            showingScore = true
         }
-        showingScore = true
+    }
+    func reset() {
+        askQuestion()
+        score = 0
     }
     
     func askQuestion() {
