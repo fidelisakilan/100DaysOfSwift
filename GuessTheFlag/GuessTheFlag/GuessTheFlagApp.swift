c5f4d716662211a439d7cab95cb37d45426e7701
Fidelis Akilan <fidelisakilan@proton.me>
2025-05-23 17:33:50 -0500

    gradient

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Fidelis Akilan on 5/23/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
