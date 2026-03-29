bdb36e37d29bd56ab0c96a7065fe2c9c0bc4442d
Fidelis Akilan <fidelisakilan@proton.me>
2026-03-29 14:34:17 -0500

    day 72

---
+++ BucketList/BucketList/UserLocationView-ContentView.swift
@@ -0,0 +1,75 @@
+//
+//  UserLocationView-ContentView.swift
+//  BucketList
+//
+//  Created by Fidelis Akilan on 3/29/26.
+//
+
+import Foundation
+import MapKit
+import LocalAuthentication
+
+extension UserLocationView {
+    @Observable
+    class ViewModel {
+        var isUnlocked = false
+        var locations = [CLocation]()
+        var selectedPoint: CLocation?
+        let savePath = URL.documentsDirectory.appending(path: "SavedPlaces")
+        
+        init() {
+            do {
+                let data = try Data(contentsOf: savePath)
+                locations = try JSONDecoder().decode([CLocation].self, from: data)
+            } catch {
+                locations = []
+            }
+        }
+        
+        func authenticate() {
+            let context = LAContext()
+            var error: NSError?
+            
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                let reason = "Please authenticate to view your places"
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, error in
+                    if success {
+                        self.isUnlocked = true
+                        print("is unlocked")
+                    } else {
+                        print("Failed to unlock \(error?.localizedDescription)")
+                        // failed to authenticate
+                    }
+                }
+            } else {
+                print("Failed to unlock \(error?.localizedDescription)")
+                //no biometrics
+            }
+            
+        }
+        
+        func save() {
+            do {
+                let data = try JSONEncoder().encode(locations)
+                try data.write(to: savePath, options: [.atomic, .completeFileProtection])
+            } catch {
+                print("Error during save")
+            }
+        }
+        
+        func addLocation(at point: CLLocationCoordinate2D) {
+            let newLocation = CLocation(id: UUID(), name: "New Pin", description: "", latitude: point.latitude, longitude: point.longitude)
+            locations.append(newLocation)
+            save()
+        }
+        
+        func update(location: CLocation) {
+            guard let selectedPoint else { return }
+            if let index = locations.firstIndex(of: selectedPoint) {
+                locations[index] = location
+                save()
+            }
+        }
+    }
+    
+}
--- BucketList/BucketList/UserLocationView.swift
+++ BucketList/BucketList/UserLocationView.swift
@@ -1,10 +1,3 @@
-//
-//  UserLocationView.swift
-//  BucketList
-//
-//  Created by Fidelis Akilan on 3/27/26.
-//
-
 import MapKit
 import SwiftUI
 
@@ -15,39 +8,43 @@ struct UserLocationView: View {
             span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
         )
     )
-    @State private var locations = [CLocation]()
-    @State private var selectedPoint: CLocation?
+    @State private var viewModel = ViewModel()
     var body: some View {
-        MapReader { proxy in
-            Map(initialPosition: startPosition) {
-                ForEach(locations) { location in
-                    Annotation(location.name, coordinate: location.coordinate) {
-                        Image(systemName: "star.circle")
-                            .resizable()
-                            .foregroundStyle(.red)
-                            .background(.white)
-                            .frame(width: 44, height: 44)
-                            .clipShape(.circle)
-                            .onLongPressGesture(minimumDuration: 0.1) {
-                                selectedPoint = location
-                            }
+        if viewModel.isUnlocked {
+            MapReader { proxy in
+                Map(initialPosition: startPosition) {
+                    ForEach(viewModel.locations) { location in
+                        Annotation(location.name, coordinate: location.coordinate) {
+                            Image(systemName: "star.circle")
+                                .resizable()
+                                .foregroundStyle(.red)
+                                .background(.white)
+                                .frame(width: 44, height: 44)
+                                .clipShape(.circle)
+                                .onLongPressGesture(minimumDuration: 0.1) {
+                                    viewModel.selectedPoint = location
+                                }
+                        }
+                    }
+                }
+                .mapStyle(.hybrid)
+                .onTapGesture { position in
+                    if let coordinate = proxy.convert(position, from: .local) {
+                        viewModel.addLocation(at: coordinate)
+                    }
+                }
+                .sheet(item: $viewModel.selectedPoint) { point in
+                    EditLocationView(location: point) {
+                        viewModel.update(location: $0)
                     }
                 }
             }
-            .mapStyle(.hybrid)
-            .onTapGesture { position in
-                if let coordinate = proxy.convert(position, from: .local) {
-                    let newLocation = CLocation(id: UUID(), name: "New Pin", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
-                    locations.append(newLocation)
-                }
-            }
-            .sheet(item: $selectedPoint) { point in
-                EditLocationView(location: point) { newLocation in
-                    if let index = locations.firstIndex(of: point) {
-                        locations[index] = newLocation
-                    }
-                }
-            }
+        } else {
+            Button("Authenticate", action: viewModel.authenticate)
+                .padding()
+                .background(.blue)
+                .foregroundStyle(.white)
+                .clipShape(.capsule)
         }
     }
 }
